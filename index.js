010248e8d6ce4e0a47133fa0afd6af2a788f07aa
cl-aaron <aaron.elligsen@creativelive.com>
2014-07-16 11:32:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,139 @@
+
+var constants = {
+  TYPE_MISMATCH: {
+      matching_types: false,
+      similarity: 0,
+      deep_equal: false
+  }
+};
+
+function compare_literals(lhs,rhs) {
+  if(lhs === rhs) {
+    return {
+      matching_types: true,
+      similarity: 1,
+      deep_equal: true
+    };
+  }else{
+    return {
+      matching_types: true,
+      similarity: 0,
+      deep_equal: false 
+    };
+  }
+};
+
+function compare_arrays(lhs, rhs) {
+  var length = Math.max(lhs.length, rhs.length);
+  var matching = 0;
+  var total_properties = length;
+  var deep_equal = false;
+  for(var i = 0; i < length; i++) {
+    if(i in lhs && i in rhs) {
+      var member_comparison = fuzzyEqual(lhs[i],rhs[i]);
+      if(member_comparison.matching_types && member_comparison.deep_equal) {
+        matching++;
+      }
+    }
+  }
+  var similarity = 0;
+  if(length > 0) {
+    similarity = matching/length;
+    if(length === matching) {
+      deep_equal = true;
+    }
+  }
+  return {
+    matching_types: true,
+    property_count: length,
+    matching: matching,
+    similarity: similarity,
+    deep_equal: deep_equal
+  };
+};
+
+function compare_objects(lhs,rhs) {
+  var total_properties = 0;
+  var matching = 0;
+  var similarity = 0;
+  var matching_props = {};
+  var leftside_props = [];
+  var rightside_props = [];
+  var differing_props = [];
+  var deep_equal = false;
+  for(var prop in lhs) {
+      total_properties++;
+      if(prop in rhs) {
+        matching_props[prop] = true;
+        var comparison = fuzzyEqual(lhs[prop],rhs[prop]);
+        if(comparison.matching_types && comparison.deep_equal) {
+          matching++;
+        }else{
+          differing_props.push[prop];
+        }
+      }else{
+        leftside_props.push(prop);
+        differing_props.push[prop];
+      }
+  }
+
+  for(var rprop in rhs) {
+    if(rprop in matching_props) {
+      continue;
+    }else{
+      total_properties++;
+      rightside_props.push(rprop);
+      differing_props.push[rprop];
+    }
+  }
+
+  if(total_properties > 0 ) {
+    similarity = matching/total_properties;
+    if(total_properties === matching) {
+      deep_equal = true;
+    }
+  }
+  return {
+    matching_types: true,
+    property_count: total_properties,
+    matching: matching,
+    similarity: similarity,
+    deep_equal: deep_equal,
+    differing_properties: differing_props,
+    left_only: leftside_props,
+    right_only: rightside_props
+  }
+};
+
+
+function fuzzyEqual(lhs, rhs) {
+  var left_hand_type = typeof lhs;
+  var right_hand_type = typeof rhs;
+  var not_objects = (left_hand_type != "object") && (right_hand_type != "object");
+  if(left_hand_type === right_hand_type && not_objects) {
+    // they're similar type
+    // if they're literals
+    return compare_literals(lhs,rhs);
+  }else if(left_hand_type === right_hand_type) {
+    // if they're object or arrays
+    var left_hand_array = Array.isArray(lhs);
+    var right_hand_array = Array.isArray(rhs);
+    var left_hand_null = lhs === null;
+    var right_hand_null = rhs === null;
+    if(left_hand_null && right_hand_null) {
+      return compare_literals(lhs,rhs);
+    }else if(left_hand_array === right_hand_array && !(left_hand_null || right_hand_null)) {
+      if(left_hand_array) {
+        return compare_arrays(lhs, rhs); 
+      }else{
+        return compare_objects(lhs, rhs); 
+      }
+    }else{
+      return constants.TYPE_MISMATCH; 
+    }
+  }else{
+    return constants.TYPE_MISMATCH; 
+  }
+}
+
+module.exports = fuzzyEqual;
---
+++ tests/fuzzytest.js
@@ -0,0 +1,75 @@
+var fuzzyEqual = require('../index.js');
+var should = require('should');
+
+describe("Fuzzy Equal",function() {
+  var sampleobject = {a:1,b:2};
+  var sampleboolean = true;
+  var samplenumber = 7;
+  var samplestring = "toast";
+  var samplearray = [1,2,3,4,5];
+  it("should not compare objects of different types",function(){
+    fuzzyEqual(samplenumber,sampleboolean).matching_types.should.not.be.true; // number boolean
+    fuzzyEqual(samplenumber,samplestring).matching_types.should.not.be.true; // number string 
+    fuzzyEqual(samplenumber,sampleobject).matching_types.should.not.be.true; // number object 
+    fuzzyEqual(samplenumber,samplearray).matching_types.should.not.be.true; // number array 
+
+    fuzzyEqual(sampleboolean,samplestring).matching_types.should.not.be.true; //boolean string
+    fuzzyEqual(sampleboolean,sampleobject).matching_types.should.not.be.true; //boolean object
+    fuzzyEqual(sampleboolean,samplearray).matching_types.should.not.be.true; //boolean array 
+
+    fuzzyEqual(samplestring,sampleobject).matching_types.should.not.be.true; //string object
+    fuzzyEqual(samplestring,samplearray).matching_types.should.not.be.true; //string array
+
+    fuzzyEqual(sampleobject,samplearray).matching_types.should.not.be.true; //object array 
+  });
+
+  it("should compare objects of similiar types", function() {
+    fuzzyEqual(sampleobject,sampleobject).matching_types.should.be.true;
+    fuzzyEqual(sampleboolean,sampleboolean).matching_types.should.be.true;
+    fuzzyEqual(samplenumber,samplenumber).matching_types.should.be.true;
+    fuzzyEqual(samplestring,samplestring).matching_types.should.be.true;
+    fuzzyEqual(samplearray,samplearray).matching_types.should.be.true;
+  });
+
+  it("should compare objects of similiar literal types and should show deep equals", function() {
+    fuzzyEqual(sampleboolean,true).deep_equal.should.be.true;
+    fuzzyEqual(sampleboolean,false).deep_equal.should.be.false;
+    fuzzyEqual(samplenumber,7).deep_equal.should.be.true;
+    fuzzyEqual(samplenumber,8).deep_equal.should.be.false;
+    fuzzyEqual(samplestring,"toast").deep_equal.should.be.true;
+    fuzzyEqual(samplestring,"bagel").deep_equal.should.be.false;
+  });
+  it("should compare arrays and show deep equals",function(){
+    fuzzyEqual(samplearray,[1,2,3,4,5]).deep_equal.should.be.true;
+    fuzzyEqual(samplearray,[1,2,3,4]).deep_equal.should.be.false;
+  });
+
+  it("should compare objects and show deep equals",function() {
+    fuzzyEqual(sampleobject,{a:1,b:2}).deep_equal.should.be.true;
+    fuzzyEqual({b:1},{c:1}).deep_equal.should.be.false;
+    fuzzyEqual({a:1},{a:1,b:1}).deep_equal.should.be.false;
+  });
+
+  it("should compare arrays and objects recursively",function(){
+    fuzzyEqual([{a:1},{b:1}],[{a:1},{b:1}]).deep_equal.should.be.true;
+    fuzzyEqual([{a:1},{b:1}],[{a:1},{c:1}]).deep_equal.should.be.false;
+    fuzzyEqual({a:[1,2,3]},{a:[1,2,3]}).deep_equal.should.be.true;
+  });
+
+  it("should compare things to null correctly", function() {
+    fuzzyEqual(sampleobject, null).matching_types.should.be.false;
+    fuzzyEqual(sampleboolean,null).matching_types.should.be.false;
+    fuzzyEqual(samplenumber, null).matching_types.should.be.false;
+    fuzzyEqual(samplestring, null).matching_types.should.be.false;
+    fuzzyEqual(samplearray,  null).matching_types.should.be.false;
+  });
+
+  it("should compare things to undefined correctly", function() {
+    fuzzyEqual(sampleobject, undefined).matching_types.should.be.false;
+    fuzzyEqual(sampleboolean,undefined).matching_types.should.be.false;
+    fuzzyEqual(samplenumber, undefined).matching_types.should.be.false;
+    fuzzyEqual(samplestring, undefined).matching_types.should.be.false;
+    fuzzyEqual(samplearray,  undefined).matching_types.should.be.false;
+  });
+
+});
